470f0d8cea9725fdb2c5f60e9514a33be4768ae2
Theotherguy <theotherguy@getnada.com>
2023-01-07 21:22:15 +0100

    update xvideos script

--- mediaplugins/xvideos_com/XVideo.js
+++ mediaplugins/xvideos_com/XVideo.js
@@ -14,14 +14,14 @@ module.exports = XVideos = {
         const movies = root.querySelectorAll('.thumb-block');
         for (const movie of movies) {
             const fileNameEL = movie.querySelector('.thumb-under > p > a');
-            if (!fileNameEL.getAttribute("title")) continue;
+            if (!fileNameEL?.getAttribute("title")) continue;
             result.push({
                 source: "xvideos.com",
-                title: fileNameEL.getAttribute("title").trim(),
+                title: fileNameEL?.getAttribute("title").trim(),
                 scrapper_class_name: "XVideos",
-                media_link: XVideos.buildFullUrl(fileNameEL.getAttribute("href")) + "%23show-related",
+                media_link: XVideos.buildFullUrl(fileNameEL?.getAttribute("href")) + "%23show-related",
                 type: "movie",
-                preview_image: movie.querySelector('.thumb-inside > .thumb > a > img').getAttribute("data-src")
+                preview_image: movie.querySelector('.thumb-inside > .thumb > a > img')?.getAttribute("data-src")
             });
         }
         cb(result);
@@ -41,7 +41,7 @@ module.exports = XVideos = {
         const genreEls = root.querySelectorAll(".video-tags-list > ul > li");
         for (const genreEl of genreEls) {
             if (genreEl.classList.contains("model")) {
-                const href = genreEl.querySelector("a").getAttribute("href");
+                const href = genreEl.querySelector("a")?.getAttribute("href");
                 result.casts.push(href/*href.substring(href.lastIndexOf("/")+1)*/);
                 continue;
             };
@@ -52,14 +52,14 @@ module.exports = XVideos = {
         for (const movie of movies) {
             const fileNameEL = movie.querySelector('.thumb-under > p > a');
             const imageEl = movie.querySelector('.thumb-inside > .thumb > a > img');
-            if (!fileNameEL.getAttribute("title")) continue;
+            if (!fileNameEL?.getAttribute("title")) continue;
             similarMovies.push({
                 source: "xvideos.com",
-                title: fileNameEL.getAttribute("title").trim(),
+                title: fileNameEL?.getAttribute("title").trim(),
                 scrapper_class_name: "XVideos",
-                media_link: XVideos.buildFullUrl(fileNameEL.getAttribute("href")) + "%23show-related",
+                media_link: XVideos.buildFullUrl(fileNameEL?.getAttribute("href")) + "%23show-related",
                 type: "movie",
-                preview_image: imageEl ? imageEl.getAttribute("src") : ""
+                preview_image: imageEl ? imageEl?.getAttribute("src") : ""
             });
         }
         result.similarMovies = similarMovies;
